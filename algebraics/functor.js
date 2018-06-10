66dd4a7a9fb05813162e110b8727d98cafd63b0c
Charles Bayley <cbayley95@gmail.com>
2018-06-09 22:50:50 -0400

    Converted Functor to stampit chain and moved test

--- algebraics/functor.js
+++ algebraics/functor.js
@@ -4,44 +4,42 @@ const S = require('stampit');
 
 const assert = require('../assert');
 
-const Functor = S({
-  props: {
-    _value: null,
-  },
-  init(value) {
+const Functor = S()
+  .props({ _value: null, })
+  .init(function(value) {
     this._value = value;
-  },
-  methods: {
+  })
+  .methods({
     map(f) {
       // map :: Functor f => f a ~> (a -> b) -> f b
       return Functor(f(this._value));
     },
-  },
-});
+  })
+  .statics({
+    test() {
+      console.log('  Functor Tests');
 
-(() => {
-  console.log('  Functor Tests');
-
-  const u = Functor(2)
-  const i = x => x;
-  const f = x => x * 2;
-  const g = x => x + 1;
+      const u = Functor(2)
+      const i = x => x;
+      const f = x => x * 2;
+      const g = x => x + 1;
  
-  assert('Functor identity')
-    (u
-      .map(i)
-      ._value)
-    (u._value);
+      assert('Functor identity')
+        (u
+          .map(i)
+          ._value)
+        (u._value);
 
-  assert('Functor composition')
-    (u
-      .map(g)
-      .map(f)
-      ._value)
-    (u
-      .map(x => f(g(x)))
-      ._value);
-})();
+      assert('Functor composition')
+        (u
+          .map(g)
+          .map(f)
+          ._value)
+        (u
+          .map(x => f(g(x)))
+          ._value);
+    }
+  });
 
 module.exports = Functor;
 
