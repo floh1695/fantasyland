9cd2441398a7129aaf8fdc6e28c426e7a4a8982a
Charles Bayley <cbayley95@gmail.com>
2018-06-09 22:29:42 -0400

    Added Apply

--- index.js
+++ index.js
@@ -21,22 +21,61 @@ const Functor = S({
   },
   methods: {
     map(f) {
+      // map :: Functor f => f a ~> (a -> b) -> f b
       return Functor(f(this._value));
     },
   },
 });
 
-assert('Functor identity')
-  (Functor(5)
-    .map(x => x)
-    ._value)
-  (Functor(5)._value);
-assert('Functor composition')
-  (Functor(5)
-    .map(x => x + 1)
-    .map(x => x * 2)
-    ._value)
-  (Functor(5)
-    .map(x => (x + 1) * 2)
-    ._value);
+(() => {
+  console.log('  Functor Tests');
+
+  const u = Functor(2)
+  const i = x => x;
+  const f = x => x * 2;
+  const g = x => x + 1;
+ 
+  assert('Functor identity')
+    (u
+      .map(i)
+      ._value)
+    (u._value);
+
+  assert('Functor composition')
+    (u
+      .map(g)
+      .map(f)
+      ._value)
+    (u
+      .map(x => f(g(x)))
+      ._value);
+})();
+
+const Apply = S(Functor, {
+  methods: {
+    ap(f) {
+      // ap :: Apply f => f a ~> f (a -> b) -> f b
+      return Apply(f._value(this._value));
+    },
+  },
+});
+
+(() => {
+  console.log('  Apply Tests');
+
+  const v = Apply(2);
+  const u = Apply(x => x + 1);
+  const a = Apply(x => x * 2);
+
+  assert('Apply composition')
+    (v
+      .ap(u
+        .ap(a
+          .map(f => g => x => f(g(x)))))
+      ._value)
+    (v
+      .ap(u)
+      .ap(a)
+      ._value);
+})();
 
