1a44691b5e846b0b2eecbc6ac108a5c4a4ad9684
Charles Bayley <cbayley95@gmail.com>
2018-06-10 16:50:31 -0400

    Cleaned out old comments

--- algebraics/functor/core.js
+++ algebraics/functor/core.js
@@ -5,10 +5,6 @@ const S = require('stampit');
 const ContextCore = require('../context').core;
 
 const FunctorCore = S(ContextCore)
-  // .props({ _value: null, })
-  // .init(function(value) {
-    // this._value = value;
-  // })
   .methods({
     // map :: Functor f => f a ~> (a -> b) -> f b
     _map(type, f) {
