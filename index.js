f09a7c78f0c7b471b43b0a99ec8b8936e27a44e6
Charles Bayley <cbayley95@gmail.com>
2018-06-09 20:44:22 -0400

    Added Functor

--- index.js
+++ index.js
@@ -2,3 +2,36 @@
 
 const R = require('ramda');
 
+const assert = R.curry(function (testName, shouldBe, isThis) {
+  if (isThis === shouldBe) {
+    console.log('Passed:', testName);
+  }
+  else {
+    console.error('Failed:', testName);
+  }
+});
+
+const Functor = function (value) {
+  return {
+    _value: value,
+    /* map :: Functor f => f a ~> (a -> b) -> f b */ 
+    map: function (func) {
+      return Functor(func(this._value));
+    },
+  };
+};
+
+assert('Functor identity')
+  (Functor(5)
+    .map(x => x)
+    ._value)
+  (Functor(5)._value);
+assert('Functor composition')
+  (Functor(5)
+    .map(x => x + 1)
+    .map(x => x * 2)
+    ._value)
+  (Functor(5)
+    .map(x => (x + 1) * 2)
+    ._value);
+
